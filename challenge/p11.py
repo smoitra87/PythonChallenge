ca3357ed656f2f0e84668206ca8f880c5a3533e2
Subhodeep Moitra <subhodeep.moitra@gmail.com>
2011-10-13 22:43:16 -0700

    subselected odd,even pixels

--- challenge/p11.py
+++ challenge/p11.py
@@ -19,21 +19,16 @@ def main() :
    pix = im.load();
    (x0,y0,x1,y1) = im.getbbox() ; 
    print x0,y0,x1,y1 ;
-   im2 = im.copy();
-   pix2 = im2.load(); 
    newIm = Image.new('RGB',(x1/2,y1/2));
    newImPix = newIm.load();
-   for i in range(x0+1,x1+1,2) :
+   for i in range(x0,x1,2) :
       for j in range(y0,y1,2): 
-         newImPix[i/2,j/2] = pix[i,j];
+         if j%2 == 0 : 
+            newImPix[i/2,j/2] = pix[i+1,j];
+         else : 
+            newImPix[i/2,j/2] = pix[i,j] ;
    newIm.show();
-   (x0,y0,x1,y1) = newIm.getbbox() ; 
-   print x0,y0,x1,y1 ;
-   for i in range(x0+1,x1+1,2) :
-      for j in range(y0,y1,2): 
-         newImPix[i,j] = (0,0,0);
-   newIm.show();
-
+   pdb.set_trace();
  
 if __name__ == '__main__' :
     main()
